a1fc6f07d6f0bc961caad1a24cca9cca6a34c2fb
Anthony Eden <anthonyeden@gmail.com>
2010-02-05 11:38:27 -1000

    spaces, not tabs

--- public/javascripts/people.js
+++ public/javascripts/people.js
@@ -1,39 +1,39 @@
 var Actions = {
-	connectAddForm: function() {
-		$("form.people").submit(function() {
-			$.ajax({
-				type: 'post',
-				dataType: 'json',
-				url: $(this).action,
-				data: $(this).serialize(), 
-				success: function(json) {
-					$('ul.people').append('<li><span class="name">' + json.person.name + '</span> <a href="/people/' + json.person.id + '/delete" class="delete">delete</a></li>');
-					$('#person_name').val('');
-				}
-			});
-			return false;
-		});
-	},
-	connectDeleteLinks: function() {
-		$('ul.people').find('a.delete').live('click', function() {
-			var li = $(this).parent();
-			if (confirm('Are you sure you want to delete the person "' + li.find('span.name').text() + '"?')) {
-				$.ajax({
-					type: 'post',
-					dataType: 'json',
-					url: this.href.replace(/\/delete/, ''),
-					data: {'_method': 'delete'},
-					success: function(json) {
-						li.slideToggle();
-					}
-				});
-			}
-			return false;
-		});
-	}
+  connectAddForm: function() {
+    $("form.people").submit(function() {
+      $.ajax({
+        type: 'post',
+        dataType: 'json',
+        url: $(this).action,
+        data: $(this).serialize(), 
+        success: function(json) {
+          $('ul.people').append('<li><span class="name">' + json.person.name + '</span> <a href="/people/' + json.person.id + '/delete" class="delete">delete</a></li>');
+          $('#person_name').val('');
+        }
+      });
+      return false;
+    });
+  },
+  connectDeleteLinks: function() {
+    $('ul.people').find('a.delete').live('click', function() {
+      var li = $(this).parent();
+      if (confirm('Are you sure you want to delete the person "' + li.find('span.name').text() + '"?')) {
+        $.ajax({
+          type: 'post',
+          dataType: 'json',
+          url: this.href.replace(/\/delete/, ''),
+          data: {'_method': 'delete'},
+          success: function(json) {
+            li.slideToggle();
+          }
+        });
+      }
+      return false;
+    });
+  }
 }
 
 $(document).ready(function(){
-	Actions.connectAddForm();
-	Actions.connectDeleteLinks();
+  Actions.connectAddForm();
+  Actions.connectDeleteLinks();
 });
